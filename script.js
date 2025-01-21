1e274cd342a937924f2488063a487a7b50712b99
Aman jain <143939366+Amanjain63@users.noreply.github.com>
2025-01-21 16:22:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-prompt("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+
+
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
+console.log("This is a Blog WebPage")
